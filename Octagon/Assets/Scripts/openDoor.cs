5371dfde7ea5389236647c255cb5ae21e95cef73
Mitchell Briggs <brigmh1@student.op.ac.nz>
2019-04-27 19:40:05 +1200

    Two different versions of the project, one has opening and closing doors, other is a walk around

---
+++ Octagon/Assets/Scripts/openDoor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openDoor : MonoBehaviour
+{
+    private bool open;
+    public float positionX;
+    public float positionY;
+    public float positionZ;
+    public float rotationX;
+    public float rotationY;
+    public float rotationZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        open = true;
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        if (open)
+        {
+            transform.localPosition = new Vector3(positionX, positionY, positionZ);
+            transform.eulerAngles = new Vector3(rotationX, rotationY, rotationZ);
+        }
+        else
+        {
+            transform.localPosition = new Vector3(0, 0, 0);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        open = !open;
+    }
+}
