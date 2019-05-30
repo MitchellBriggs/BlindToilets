fac1203a4171720439b72fbc1cc4f5a1e9f2534a
Mitchell Briggs (1000043681) <BRIGMH1@student.op.ac.nz>
2019-05-30 14:40:43 +1200

    Started working on pop-up hover effects

---
+++ Octagon/Assets/Scripts/DisplayText.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DisplayText : MonoBehaviour
+{
+    public string NewText;
+    public Text TextBox;
+    private bool hovering;
+    private DateTime start;
+    private DateTime end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hovering)
+        {
+            if (DateTime.Compare(DateTime.Now, start.AddMilliseconds(1500)) > 0)
+            {
+                TextBox.text = NewText;
+                TextBox.enabled = true;
+            }
+        }
+    }
+    public void OnEnter()
+    {
+        start = DateTime.Now;
+        hovering = true;
+    }
+    public void OnExit()
+    {
+        hovering = false;
+        TextBox.enabled = false;
+    }
+}
