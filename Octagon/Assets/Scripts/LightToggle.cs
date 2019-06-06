2518df9b12d1ab175632d9dc6dc89c3ae76391db
Mitchell Briggs (1000043681) <BRIGMH1@student.op.ac.nz>
2019-06-06 14:27:46 +1200

    Lighting overhaul

---
+++ Octagon/Assets/Scripts/LightToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightToggle : MonoBehaviour
+{
+    private bool active;
+    public GameObject light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.SetActive(active);
+    }
+    public void Toggle()
+    {
+        active = !active;
+    }
+}
